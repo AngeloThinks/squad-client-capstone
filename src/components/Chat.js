301219d651fe61527a55dc9340da0ad983064260
Angelo Vazquez <angelovazquez@Angelos-MacBook-Pro.local>
2021-01-27 00:26:42 -0600

    chat body

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,5 +1,7 @@
 import { Avatar, IconButton } from "@material-ui/core";
 import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import  InsertEmoticonIcon  from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 
 import React from "react";
 import "./Chat.css";
@@ -27,14 +29,33 @@ function Chat() {
         </div>
       </div>
       <div className="chat__body">
-        <p>
-          <span>Angelo</span>
+        <p className="chat__message">
+          <span className="chat__name">Angelo</span>
+          This is a message
+          <span className="chat__timestamp">
+          {new Date().toUTCString()}
+          </span>
+        </p>
+        <p className="chat__message chat__receiver">
+          <span className="chat__name">Angelo</span>
           This is a message
           <span className="chat__timestamp">
           {new Date().toUTCString()}
           </span>
         </p>
       </div>
+        <div className="chat__footer">
+            <InsertEmoticonIcon/>
+            <form>
+                <input placeholder='Type a message' type='text'></input>
+                <button type='submit'>
+                Send
+                </button>
+            </form>
+            <MicIcon/>
+
+        </div>
+
     </div>
   );
 }
