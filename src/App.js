13d4d2b608485fa016d919b3d15d80e3fbda5f39
Angelo Vazquez <angelovazquez@Angelos-MacBook-Pro.local>
2021-01-29 04:01:47 -0600

    connected

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Chat from "./components/Chat";
-import Sidebar from ".`/components/Sidebar";
+import Sidebar from "./components/Sidebar";
+import Pusher from 'pusher-js';
 
 function App() {
+  useEffect(()=>{
+    const pusher = new Pusher('3f21b4355ecbe9949612', {
+      cluster: 'us2'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (data) => {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+
   return (
     <div className="app">
       <div className="app__body">
