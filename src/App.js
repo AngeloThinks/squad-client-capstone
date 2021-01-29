9941fad7600785f19f84614dd1238a15af200fd3
Angelo Vazquez <angelovazquez@Angelos-MacBook-Pro.local>
2021-01-29 04:26:57 -0600

    connected

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Chat from "./components/Chat";
 import Sidebar from "./components/Sidebar";
-import Pusher from 'pusher-js';
+import Pusher from "pusher-js";
+import axios from "./components/axios";
 
 function App() {
-  useEffect(()=>{
-    const pusher = new Pusher('3f21b4355ecbe9949612', {
-      cluster: 'us2'
-    });
+  const [messages, setMessages] = useState([]);
 
-    const channel = pusher.subscribe('messages');
-    channel.bind('inserted', (data) => {
-      alert(JSON.stringify(data));
+  useEffect(() => {
+    axios.get("/messages/sync").then((response) => {
+      setMessages(response.data);
     });
   }, []);
 
+  useEffect(() => {
+    const pusher = new Pusher("3f21b4355ecbe9949612", {
+      cluster: "us2",
+    });
+
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", (data) => {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+  console.log(messages);
   return (
     <div className="app">
       <div className="app__body">
